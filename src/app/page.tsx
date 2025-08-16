7c12e07bdd3e6e0372c6a000e135887bb9e2c1e3
VibeDev <size-gusty-caliber@duck.com>
2025-08-16 16:12:10 +0700

    Delete src directory
    Updating

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { EncryptorTool } from "@/components/encryptor-tool";
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen w-full flex-col items-center justify-center p-4">
-      <EncryptorTool />
-    </main>
-  );
-}
